1c6813c735158cf12f3df3628a1505f24868c52a
Lalitha0525 <124656922+Lalitha0525@users.noreply.github.com>
2023-07-06 20:31:12 +0530

    Create Input & Output - ASCII Values - II.c

---
+++ hackkerrank solved answers/Input & Output - ASCII Values - II.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int w ;
+        scanf("%d",&w);
+    printf("%c",w);
+
+        
+    return 0;
+}

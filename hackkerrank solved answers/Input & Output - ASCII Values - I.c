d26d5ce3aec2875c27ae5e86b228561899908364
Lalitha0525 <124656922+Lalitha0525@users.noreply.github.com>
2023-07-06 20:30:16 +0530

    Create Input & Output - ASCII Values - I.c

---
+++ hackkerrank solved answers/Input & Output - ASCII Values - I.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char a;
+    scanf("%c",&a);
+    printf("%d",a);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
